dd8a4b28b2fa94e8f830a35c6a50d5d1ab2d56dc
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-01 15:09:49 -0600

    Adds invoice/merchant controller for returning merchant associated wth an invoice, adds routes, adds tests, adds relationship

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
+        get ':id/merchant', to: 'merchant#index'
       end
       resources :invoices, only: [:index, :show]
     end
