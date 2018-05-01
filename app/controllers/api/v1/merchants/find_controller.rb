dfcb26232431b0b96e0e70ef23b5c114f6f21ef8
VadLusk <AdLusk@gmail.com>
2018-05-01 16:09:51 -0600

    Add show and search params

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::Merchants::FindController < ApplicationController
+  def show
+    render json: Merchant.find_by(search_params)
+  end
+
+  private
+
+    def search_params
+      params.permit(:id)
+    end
 end
