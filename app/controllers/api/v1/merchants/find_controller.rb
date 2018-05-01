0754c7692f02920f92c8406b825a0f0cf928a676
VadLusk <AdLusk@gmail.com>
2018-05-01 17:11:55 -0600

    Add find all merchants test.

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::Merchants::FindController < ApplicationController
   private
 
     def search_params
-      params.permit(:id)
+      params.permit(:id, :name)
     end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -31,4 +31,40 @@ describe 'merchants API' do
 
     expect(json["id"]).to eq(merchant.id)
   end
+  it 'finds one merchant by name' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?name=#{merchant.name}"
+
+    json = JSON.parse(response.body)
+
+    expect(json["id"]).to eq(merchant.id)
+  end
+  it 'finds one merchant by created at' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
+
+    json = JSON.parse(response.body)
+
+    expect(json["id"]).to eq(merchant.id)
+  end
+  it 'finds one merchant by updated at' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?updated_at=#{merchant.updated_at}"
+
+    json = JSON.parse(response.body)
+
+    expect(json["id"]).to eq(merchant.id)
+  end
+  it 'finds all merchants by id' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find_all?id=#{merchant.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(json["id"]).to eq(merchant.id)
+  end
 end
