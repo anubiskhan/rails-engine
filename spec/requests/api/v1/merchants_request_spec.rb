60f077efbcf0bd05d6c07edf0227b85fe36ea805
VadLusk <AdLusk@gmail.com>
2018-04-30 21:46:56 -0600

    Add merchant show spec.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 describe 'merchants API' do
-  it 'sends a list of merchants' do
+  it 'sends all merchants' do
     create_list(:merchant, 5)
 
     get '/api/v1/merchants'
 
+    merchants = JSON.parse(response.body)
+
     expect(response).to be_success
+    expect(merchants.count).to eq(5)
+  end
+  it 'sends one merchant by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant['id']).to eq(id)
   end
 end
