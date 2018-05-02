11b43f9f52b5dcf935e6e438caffffeb9a23008f
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-01 20:39:16 -0600

    Adds items with requisite tests, routind, and controllers

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Food.dish }
-    description {Faker::Food.description }
+    description {Faker::Company.catch_phrase }
     unit_price { rand(1..1000) }
     merchant
   end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -11,4 +11,48 @@ describe 'items API' do
     expect(response).to be_success
     expect(items.length).to eq(10)
   end
+  it 'sends an item' do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json["id"]).to eq(item.id)
+  end
+  it 'can find an item' do
+    item = create(:item)
+
+    get "/api/v1/items/find?id=#{item.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json['id']).to eq(item.id)
+  end
+  it 'can find multiple items' do
+    item1 = create(:item, unit_price: 112)
+    item2 = create(:item, unit_price: 112)
+
+    get "/api/v1/items/find_all?unit_price=#{item1.unit_price}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json[0]['id']).to eq(item1.id)
+    expect(json[1]['id']).to eq(item2.id)
+  end
+  it 'can find a random item' do
+    item1 = create(:item)
+    item2 = create(:item)
+    item3 = create(:item)
+
+    get '/api/v1/items/random'
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect([item1.id, item2.id, item3.id]).to include(json['id'])
+  end
 end
