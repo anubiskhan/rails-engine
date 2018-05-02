5bb0bb1c22ce1b93a05e565f90d10e7de15af666
VadLusk <AdLusk@gmail.com>
2018-05-01 18:53:33 -0600

    Add routes and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :merchants do
+      namespace :customers do
         get '/find',     to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random',   to: 'random#show'
@@ -12,14 +12,20 @@ Rails.application.routes.draw do
         get '/random',      to: 'random#show'
         get ':id/merchant', to: 'merchant#index'
       end
+      namespace :merchants do
+        get '/find',     to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random',   to: 'random#show'
+      end
       namespace :transactions do
         get '/find',     to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random',   to: 'random#show'
       end
-      resources :merchants, only: %i[index show]
+      resources :customers,    only: %i[index show]
+      resources :invoices,     only: %i[index show]
+      resources :merchants,    only: %i[index show]
       resources :transactions, only: %i[index show]
-      resources :invoices, only: [:index, :show]
     end
   end
 end
