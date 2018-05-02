466f9e454ea6ffc1249b80c9b10b8fb321f57db7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-01 19:52:22 -0600

    Adds items migration and migrates

--- app/controllers/api/v1/invoices/random_controller.rb
+++ app/controllers/api/v1/invoices/random_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Invoices::RandomController < ApplicationController
   def show
-    render json: Invoice.all.sample
+    render json: Invoice.limit(1).order("RANDOM()")
   end
 end
+id,name,description,unit_price,merchant_id,created_at,updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502004742) do
+ActiveRecord::Schema.define(version: 20180502014905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20180502004742) do
     t.integer "customer_id"
     t.integer "merchant_id"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.integer "unit_price"
+    t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
