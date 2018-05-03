26f3cac5a1d757c8bf3ef4397393a847ff00ada2
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-03 10:46:15 -0600

    Adds relationship test for customers

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 describe Customer do
+  context 'relationships' do
+    it { should have_many(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
 end
