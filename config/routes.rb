4b2a9a894d672348044c3e5e12d3ab487a3e1b23
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-03 10:42:59 -0600

    Adds funtionality for getting the invoice associated with a transaction. route, controller, test

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
         get '/find',     to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random',   to: 'random#show'
+        get '/:id/invoice', to: 'invoice#show'
       end
       resources :customers,     only: %i[index show]
       resources :invoices,      only: %i[index show]
