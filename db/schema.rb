f957880e2bcfede2af1c1e80733c21a1f6e58894
VadLusk <AdLusk@gmail.com>
2018-05-03 10:37:49 -0600

    Fix transaction test."

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180502235533) do
-=======
-ActiveRecord::Schema.define(version: 20180502033536) do
->>>>>>> Adds invoice item migration and migrates
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
